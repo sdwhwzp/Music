6b74dbd65d2e40f36f3bf66422644ddc0b27e7fc
wzp <sdwhwzp@126.com>
2019-08-02 21:40:16 +0800

    底部导航栏样式v0.1

--- music/src/components/nav/index.js
+++ music/src/components/nav/index.js
@@ -1,23 +1,30 @@
 import React from 'react'
 import router from "../../router";
 import {Route ,NavLink } from 'react-router-dom'
-
+import '../../index.css'
 export default class Nav extends React.Component{
+	constructor(props) {
+		super(props);
+		this.state={
+			positon:10
+		}
+	}
+
 	render() {
-
 		return (
-			<div>
-
+			<footer>
 				{router.map((v,i)=>{
 
 					return (
 
-							<span key={i} id={'nav'}>
-								<NavLink to={v.to}><i className={"icon iconfont "+" "+v.iconfont}></i>{v.context}</NavLink>
+						<span key={i} className={'nav'} >
+								<NavLink exact={v.exact} style={{color:'gray'}} activeStyle={{color:"green"}} to={v.to}><i className={"icon iconfont "+" "+v.iconfont}></i>{v.context}</NavLink>
 							</span>
 					)
 				})}
-			</div>
+			</footer>
+
+
 
 		)
 	}
